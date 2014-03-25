6aa3fad8ca9c5919c30a134b48f6973ebf218867
David Duke <ouroborot@gmail.com>
2014-03-24 22:34:49 -0700

    Get redis post correctly

--- stacker.go
+++ stacker.go
@@ -4,13 +4,16 @@ import (
 	"fmt"
 	"net/http"
 	"github.com/hoisie/redis"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi sthere, I love %s!", r.URL.Path[1:])
 
 	var client redis.Client
-	client.Addr = "localdocker:6379"
+	var redisHost = os.Getenv("REDIS_1_PORT_6379_TCP_ADDR")
+	var redisPort = os.Getenv("REDIS_1_PORT_6379_TCP_PORT")
+	client.Addr = redisHost + ":" + redisPort
 	var key = "hello"
 	client.Set(key, []byte("world"))
 	val, _ := client.Get("hello")
