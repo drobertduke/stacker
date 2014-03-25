b2cc54951f35761198254299b3d95421cfbb855f
David Duke <ouroborot@gmail.com>
2014-03-24 22:59:18 -0700

    Add martini web framework

--- stacker.go
+++ stacker.go
@@ -1,27 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"github.com/hoisie/redis"
 	"os"
+	"github.com/hoisie/redis"
+	"github.com/codegangsta/martini"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi sthere, I love %s!", r.URL.Path[1:])
-
-	var client redis.Client
-	var redisHost = os.Getenv("REDIS_1_PORT_6379_TCP_ADDR")
-	var redisPort = os.Getenv("REDIS_1_PORT_6379_TCP_PORT")
-	client.Addr = redisHost + ":" + redisPort
-	var key = "hello"
-	client.Set(key, []byte("world"))
-	val, _ := client.Get("hello")
-
-	fmt.Fprintf(w, "Redis says: %s", string(val))
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	m := martini.Classic()
+
+	m.Get("/", func() string {
+
+		var client redis.Client
+		var redisHost = os.Getenv("REDIS_1_PORT_6379_TCP_ADDR")
+		var redisPort = os.Getenv("REDIS_1_PORT_6379_TCP_PORT")
+		client.Addr = redisHost + ":" + redisPort
+		var key = "hello"
+		client.Set(key, []byte("world"))
+		val, _ := client.Get("hello")
+
+		return "martini " + string(val)
+	});
+
+	//m.Run()
+	http.ListenAndServe(":8080", m)
 }
+
