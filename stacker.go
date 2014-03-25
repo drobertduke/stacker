a34a09088ff6609fc5775b1a7b68483cb7da7be6
David Duke <ouroborot@gmail.com>
2014-03-24 20:19:40 -0700

    Env management and Redis test

--- stacker.go
+++ stacker.go
@@ -3,10 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/hoisie/redis"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hi sthere, I love %s!", r.URL.Path[1:])
+
+	var client redis.Client
+	var key = "hello"
+	client.Set(key, []byte("world"))
+	val, _ := client.Get("hello")
+
+	fmt.Fprintf(w, val, r.URL.Path[1:])
 }
 
 func main() {
