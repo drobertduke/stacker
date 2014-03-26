ebe540e0ffb6bc7d5aabdc38f01ac54e264bdc12
David Duke <ouroborot@gmail.com>
2014-03-25 22:05:06 -0700

    Switch to redigo

--- stacker.go
+++ stacker.go
@@ -3,27 +3,71 @@ package main
 import (
 	"net/http"
 	"os"
-	"github.com/hoisie/redis"
+	"log"
 	"github.com/codegangsta/martini"
+	"github.com/garyburd/redigo/redis"
 )
 
 func main() {
+	var redisHost = os.Getenv("REDIS_1_PORT_6379_TCP_ADDR")
+	var redisPort = os.Getenv("REDIS_1_PORT_6379_TCP_PORT")
+	c, err := redis.Dial("tcp", redisHost + ":" + redisPort)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	m := martini.Classic()
+
+	c.Do("SET", "hello", "world")
 
 	m.Get("/", func() string {
 
-		var client redis.Client
-		var redisHost = os.Getenv("REDIS_1_PORT_6379_TCP_ADDR")
-		var redisPort = os.Getenv("REDIS_1_PORT_6379_TCP_PORT")
-		client.Addr = redisHost + ":" + redisPort
-		var key = "hello"
-		client.Set(key, []byte("world"))
-		val, _ := client.Get("hello")
-
-		return "martini " + string(val)
+		val, _ := c.Do("GET", "hello")
+		bytes, ok := val.([]byte)
+		if !ok {
+			log.Fatal("Whoops")
+		}
+		return "martini " + string(bytes)
 	});
 
-	//m.Run()
+	// API
+
+	// USERS
+
+	m.Get("/users", func() string {
+		return "list of users"
+	})
+
+	m.Get("/users/:userId", func(params martini.Params) string {
+		return "DETAIL FOR USER " + params["userId"]
+	})
+
+	m.Get("/users/:userId/tasks", func(params martini.Params) string {
+		return "TASKS FOR USER " + params["userId"]
+	})
+
+	m.Post("/users", func(params martini.Params) string {
+		return "POSTED USER"
+	})
+
+	m.Put("/users/:userId", func(params martini.Params) string {
+		return "PUT USER " + params["userId"]
+	})
+
+	// TASKS
+
+	m.Get("/tasks", func() string {
+		return "list of tasks"
+	})
+
+	m.Post("/tasks", func() string {
+		return "POSTED TASK"
+	})
+
+	m.Put("/tasks/:taskId", func(params martini.Params) string {
+		return "PUT TASK" + params["taskId"]
+	})
+
 	http.ListenAndServe(":8080", m)
 }
 
