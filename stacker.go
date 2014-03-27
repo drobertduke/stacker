f5b6c30819b0b279f3dbe280b2b89493a80279f0
David Duke <ouroborot@gmail.com>
2014-03-26 22:42:28 -0700

    Add zoom ORM and martini binding

--- stacker.go
+++ stacker.go
@@ -5,14 +5,45 @@ import (
 	"os"
 	"log"
 	"github.com/codegangsta/martini"
+	"github.com/albrow/zoom"
 	"github.com/garyburd/redigo/redis"
+	"github.com/martini-contrib/binding"
 )
+
+type User struct {
+	FullName string
+	Username string
+	zoom.DefaultData
+}
+
+type UserPost struct {
+	FullName string `form:"fullName" json:"fullName" binding:"required"`
+	Username string `form:"username" json:"username" binding:"required"`
+}
+
+func (up UserPost) Validate(errors *binding.Errors, req *http.Request) {
+	lengthMax := 20
+	if len(up.FullName) > lengthMax {
+		errors.Fields["fullName"] = "Too long, should be less than " + string(lengthMax)
+	}
+	if len(up.Username) > lengthMax {
+		errors.Fields["username"] = "Too long, should be less than " + string(lengthMax)
+	}
+}
 
 func main() {
 	var redisHost = os.Getenv("REDIS_1_PORT_6379_TCP_ADDR")
 	var redisPort = os.Getenv("REDIS_1_PORT_6379_TCP_PORT")
-	c, err := redis.Dial("tcp", redisHost + ":" + redisPort)
-	if err != nil {
+
+	zoomConfig := &zoom.Configuration {
+		Address: redisHost + ":" + redisPort,
+		Network: "tcp",
+	}
+	zoom.Init(zoomConfig)
+
+	c, _ := redis.Dial("tcp", redisHost + ":" + redisPort)
+
+	if err := zoom.Register(&User{}); err != nil {
 		log.Fatal(err)
 	}
 
@@ -46,7 +77,14 @@ func main() {
 		return "TASKS FOR USER " + params["userId"]
 	})
 
-	m.Post("/users", func(params martini.Params) string {
+	m.Post("/users", binding.Bind(UserPost{}), func(params martini.Params) string {
+		user := &User {
+			FullName: params["fullName"],
+			Username: params["username"],
+		}
+		if err := zoom.Save(user); err != nil {
+			log.Fatal(err)
+		}
 		return "POSTED USER"
 	})
 
